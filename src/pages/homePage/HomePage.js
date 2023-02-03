6933d3d423b6135effa45343296426a4fb203132
Elene M <emetreve@gmail.com>
2023-02-03 16:57:17 +0400

    created HomePage view

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,7 +1,22 @@
+import styles from "./HomePage.module.css";
+
+import logo from "../../images/homeLogo.png";
+import line from "../../images/line.png";
+import background from "../../images/homeBackground.png";
+
 function HomePage() {
   return (
     <>
-      <div>Home Page</div>
+      <div className={styles.wrapper}>
+        <img
+          src={background}
+          className={styles.background}
+          alt="background pattern"
+        />
+        <img src={logo} className={styles.logo} alt="redberry logo" />
+        <img src={line} className={styles.line} alt="divider" />
+        <button className={styles.button}>რეზიუმეს დამატება</button>
+      </div>
     </>
   );
 }
